147e52a260ff5a8fccb65f75c7bded62e6303da1
leandro-frota <leandromfrota@gmail.com>
2025-01-24 00:16:34 -0300

    Refactored Login page

--- src/pages/login.jsx
+++ src/pages/login.jsx
@@ -2,6 +2,7 @@ import  { useState,useContext } from 'react';
 import { useNavigate } from 'react-router-dom';
 import { login } from '../service/apiService';
 import { AuthContext } from '../context/AuthContentx.jsx';
+import { Container } from 'react-bootstrap';
 
 
 
@@ -34,32 +35,37 @@ const Login = () => {
     };
 
     return (
-        <div className="login-container">
-            <h2>Login</h2>
-            <form onSubmit={handleSubmit}>
-                <div className="form-group">
-                    <label htmlFor="email">Email:</label>
-                    <input
-                        type="email"
-                        id="email"
-                        value={email}
-                        onChange={(e) => setEmail(e.target.value)}
-                        required
-                    />
-                </div>
-                <div className="form-group">
-                    <label htmlFor="password">Password:</label>
-                    <input
-                        type="password"
-                        id="password"
-                        value={password}
-                        onChange={(e) => setPassword(e.target.value)}
-                        required
-                    />
-                </div>
-                <button type="submit">Login</button>
-            </form>
-        </div>
+        <Container className="d-flex justify-content-center align-items-center min-vh-100 min-vw-100">
+            <div className="d-flex flex-column gap-4 p-4 border rounded shadow-lg bg-white">
+                <h2 className='text-center'>Login</h2>
+                <form onSubmit={handleSubmit}>
+                    <div className="form-group">
+                        <label className='form-label' htmlFor="email">Email:</label>
+                        <input
+                            className='form-control'
+                            type="email"
+                            id="email"
+                            value={email}
+                            onChange={(e) => setEmail(e.target.value)}
+                            required
+                        />
+                    </div>
+                    <div className='form-group'>
+                        <label className='form-label' htmlFor="password">Password:</label>
+                        <input
+                            className='form-control'
+                            type="password"
+                            id="password"
+                            value={password}
+                            onChange={(e) => setPassword(e.target.value)}
+                            required
+                            aria-describedby="passwordHelpBlock"
+                        />
+                    </div>
+                    <button type="submit" className="btn btn-success mt-3 w-100">Login</button>
+                </form>
+            </div>
+        </Container>
     );
 };
 
